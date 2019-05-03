909f5372db17606dfb92550a653fe34cffab3b54
BeemerBiker <josephy@stateson.net>
2019-05-03 11:34:01 -0500

    use arglist to supply IP and password

--- RPCupdate/Program.cs
+++ RPCupdate/Program.cs
@@ -10,9 +10,30 @@ namespace RPCupdate
 {
     class Program
     {
+
+        static string strIP = "";
+        static string strPW = "";
+        static int bPort = 31416;
+
         static void Main(string[] args)
         {
 
+            if(args.Length < 1)
+            {
+                Console.WriteLine("usage: <ipaddress> <password>\nExample: RPCupdate \"192.168.1.225\"");
+                Console.WriteLine("or -  RPCupdate \"192.168.1.225\" \"your password\"");
+                Console.WriteLine("use quotes if space in password");
+            }
+            if(args.Length > 0)
+            {
+                strIP = args[0];
+            }
+            if(args.Length > 1)
+            {
+                strPW = args[1];
+            }
+            //Console.WriteLine(strIP);
+            //Console.WriteLine(strPW);
             GetMWproj();
             var t = Task.Run(async delegate
             {
@@ -24,16 +45,14 @@ namespace RPCupdate
 
         }
 
-        static string strPassword = "";
-
 
         static async Task GetMWproj()
         {
 
             using (RpcClient rpcClient = new RpcClient())
             {
-                await rpcClient.ConnectAsync("192.168.1.251", 31416);
-                bool authorized = await rpcClient.AuthorizeAsync(strPassword);
+                await rpcClient.ConnectAsync(strIP, bPort);
+                bool authorized = await rpcClient.AuthorizeAsync(strPW);
                 CoreClientState ccs = await rpcClient.GetStateAsync();
                 foreach (Project p in ccs.Projects)
                 {
